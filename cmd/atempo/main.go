2c1407780f6809bf256b5416f17fecfaa1afda65
Mark Nichols <mark@zanshin.net>
2022-11-11 16:48:26 -0600

    Added in database to main

--- cmd/atempo/main.go
+++ cmd/atempo/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/zanshin/atempo/pkg/config"
 	l "github.com/zanshin/atempo/pkg/logger"
+	"github.com/zanshin/atempo/pkg/model"
 )
 
 var (
@@ -28,4 +30,41 @@ func main() {
 
 	l.Info.Println("Configuration read from file system.")
 	fmt.Println(conf)
+
+	// Connect to the database
+	db, err := model.DBConnect("", conf.DbConfig)
+	if err != nil {
+		l.Error.Fatal("Connection to MySQL failed. Exiting.")
+	}
+
+	l.Info.Println("Successfully connected to MySQL")
+
+	// Create database if it doesn't already exist
+	dbname := "testdb2"
+	res, err := model.DBCreate(db, dbname)
+	if err != nil {
+		l.Error.Fatal("Database creation failed. Exiting")
+	}
+
+	// Test result of creation for number of row affected.
+	no, err := res.RowsAffected()
+	if err != nil {
+		l.Error.Printf("Error %s when fetching affected rows\n", err)
+		l.Error.Fatal("Unable to get affected rows. Exiting")
+	}
+
+	l.Info.Printf("Rows affected: %d\n", no)
+
+	// Set connection pool options
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(20)
+	db.SetConnMaxLifetime(time.Minute * 5)
+
+	// Verify the connection
+	err = model.DBPing(db)
+	if err != nil {
+		l.Error.Fatal("Unable to ping database. Exiting")
+	}
+
+	l.Info.Printf("Connected to DB %s successfully\n", dbname)
 }
