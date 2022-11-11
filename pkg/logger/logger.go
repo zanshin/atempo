df6af460ba963bea049b8efdb1fb7c0676fcd5d5
Mark Nichols <mark@zanshin.net>
2022-11-11 17:23:13 -0600

    Colorized log message tags

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -11,6 +12,13 @@ var (
 	Error   *log.Logger
 )
 
+const (
+	Red    string = "\033[;31m"
+	Yellow string = "\033[;33m"
+	Green  string = "\033[;32m"
+	Reset  string = "\033[;0m"
+)
+
 func Setup(name string) {
 
 	file, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
@@ -18,7 +26,12 @@ func Setup(name string) {
 		log.Fatal(err)
 	}
 
-	Info = log.New(file, "INFO   : ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warning = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(file, "ERROR  : ", log.Ldate|log.Ltime|log.Lshortfile)
+	// es := fmt.Sprintf(Red, "Error   : ", Reset)
+	es := fmt.Sprintf("%s%s%s", string(Red), "Error   : ", string(Reset))
+	ws := fmt.Sprintf("%s%s%s", string(Yellow), "Warning : ", string(Reset))
+	is := fmt.Sprintf("%s%s%s", string(Green), "Info    : ", string(Reset))
+
+	Info = log.New(file, is, log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(file, ws, log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(file, es, log.Ldate|log.Ltime|log.Lshortfile)
 }
