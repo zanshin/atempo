00bd2baad91a6460d91327fb00f2b37bb724b04f
Mark Nichols <mark@zanshin.net>
2022-11-12 20:31:58 -0600

    Added Get functions to model

--- internal/model/hrefClicks.go
+++ internal/model/hrefClicks.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"database/sql"
+	_ "database/sql"
+
+	l "github.com/zanshin/atempo/internal/logger"
+)
+
 type HrefClick struct {
 	IPAddress  string `json:"ipAddress"`
 	URL        string `json:"url"`
@@ -8,4 +15,36 @@ type HrefClick struct {
 	HrefRight  int    `json:"hrefRight"`
 	HrefBottom int    `json:"hrefBottom"`
 	HrefLeft   int    `json:"hrefLeft"`
+}
+
+type HrefClicks struct {
+	HrefClicks []HrefClick `json:"hrefClick"`
+}
+
+func GetHrefClicks(dbConn sql.DB) HrefClicks {
+	sqlStatement := "SELECT ipAddress, url, href, hrefTop, hrefRight, hrefBottom, hrefLeft"
+
+	rows, err := dbConn.Query(sqlStatement)
+	if err != nil {
+		l.Error.Printf("Unable to select HrefClicks. Reason: %s", err)
+		return HrefClicks{}
+	}
+
+	defer rows.Close()
+
+	result := HrefClicks{}
+	for rows.Next() {
+		hc := HrefClick{}
+
+		err = rows.Scan(&hc.IPAddress, &hc.URL, &hc.Href, &hc.HrefTop, &hc.HrefRight, &hc.HrefBottom, &hc.HrefLeft)
+		if err != nil {
+			l.Error.Printf("Unable to scan row into PaveView struct. Reason: %s", err)
+			return HrefClicks{}
+		}
+
+		result.HrefClicks = append(result.HrefClicks, hc)
+	}
+
+	return result
+
 }
--- internal/model/pageViews.go
+++ internal/model/pageViews.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"database/sql"
+	_ "database/sql"
+
+	l "github.com/zanshin/atempo/internal/logger"
+)
+
 type PageView struct {
 	Domain       string `json:"domain"`
 	IPAddress    string `json:"ipAddress"`
@@ -7,4 +14,36 @@ type PageView struct {
 	UserAgent    string `json:"userAgent"`
 	ScreenHeight int    `json:"screenHeight"`
 	ScreenWidth  int    `json:"screenWidth"`
+}
+
+type PageViews struct {
+	PageViews []PageView `json:"pageView"`
+}
+
+func GetPageViews(dbConn sql.DB) PageViews {
+	sqlStatement := "SELECT domain, ipAddress, url, userAgent, screenHeight, screenWidth"
+
+	rows, err := dbConn.Query(sqlStatement)
+	if err != nil {
+		l.Error.Printf("Unable to select PageViews. Reason: %s", err)
+		return PageViews{}
+	}
+
+	defer rows.Close()
+
+	result := PageViews{}
+	for rows.Next() {
+		pv := PageView{}
+
+		err = rows.Scan(&pv.Domain, &pv.IPAddress, &pv.URL, &pv.UserAgent, &pv.ScreenHeight, &pv.ScreenWidth)
+		if err != nil {
+			l.Error.Printf("Unable to scan row into PaveView struct. Reason: %s", err)
+			return PageViews{}
+		}
+
+		result.PageViews = append(result.PageViews, pv)
+	}
+
+	return result
+
 }
